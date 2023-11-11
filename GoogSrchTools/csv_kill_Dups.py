af25ad0e1d2f626701a8e71532c73b06155253e9
vladmetc <vvmedian@gmail.com>
2023-11-11 22:46:24 +0300

    csv_kill_Dups.py edited

--- GoogSrchTools/csv_kill_Dups.py
+++ GoogSrchTools/csv_kill_Dups.py
@@ -4,12 +4,12 @@ Refine news articles csv file: kill duplicates, handle line-breaks.
 import csv
 
 
-def csv_kill_dups(read_file, write_file, key_unique):
+def csv_kill_dups(read_file, write_file, unique_text_key) -> None:
     """
     Rewrite csv file killing duplicated values of 'key_unique'
     :param read_file:  path to input csv file - str
     :param write_file:   path to output csv file - str
-    :param key_unique:  key of the values that to be unique - str
+    :param unique_text_key:  key of the values that to be unique - str
     """
     with (open(read_file) as in_file,
           open(write_file, "w", newline='') as out_file):
@@ -18,7 +18,7 @@ def csv_kill_dups(read_file, write_file, key_unique):
         csv.DictWriter(out_file, fieldnames=fieldnames).writeheader()
         data = csv.DictReader(in_file, fieldnames=fieldnames)
         for i, row in enumerate(data):
-            text = row[key_unique]  # The 'text' field to have unique values
+            text = row[unique_text_key]  # e.g. 'text' field of Google-Search-news
             if text in unique:
                 continue
             unique.append(text)
@@ -26,7 +26,7 @@ def csv_kill_dups(read_file, write_file, key_unique):
             for k, v in row.items():
                 # Remove unneeded newlines
                 refined_row[k] = v.replace('\n', '')
-            # Write the unique-text-value row in the out_file
+            # Write rows with unique values in the out_file
             csv.DictWriter(out_file, fieldnames=fieldnames).writerow(refined_row)
         # print("unique:\n", unique)
 
@@ -35,4 +35,4 @@ if __name__ == '__main__':
     csv_kill_dups(
         read_file='../Kraken/re_csv/articles_data.csv',
         write_file='../Kraken/re_csv/articles_data_noDups.csv',
-        key_unique='text')
+        unique_text_key='text')
